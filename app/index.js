0604827910293c234f7a0505159fcfe80cc9fcf8
Anya <anya@DNa1ca940.SUNet>
2023-12-06 01:46:14 -0800

    created UI for front page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 import { Link } from "expo-router";
@@ -11,31 +11,86 @@ const supabase = createClient(
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Link
-        href={{
-          pathname: "/CreateGroupScreen",
-          params: {
-            title: "Create Group",
-          },
-        }}
-        asChild
-      >
-        <Text>Create a New Session</Text>
-      </Link>
-      <Link
-        href={{
-          pathname: "/JoinSessionScreen",
-          params: {
-            title: "Join Session",
-          },
-        }}
-        asChild
-      >
-        <Text>Join an Existing Session</Text>
-      </Link>
+    <View style={styles.container}>
+      <View style={styles.headerSpacer} />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>We Choose</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonWrapper}>
+          <Link
+            href={{
+              pathname: "/CreateGroupScreen",
+              params: {
+                title: "Create Group",
+              },
+            }}
+            asChild
+          >
+            <Text style={styles.buttonText}>Create a New Session</Text>
+          </Link>
+        </View>
+
+        <View style={styles.buttonWrapper}>
+          <Link
+            href={{
+              pathname: "/JoinSessionScreen",
+              params: {
+                title: "Join Session",
+              },
+            }}
+            asChild
+          >
+            <Text style={styles.buttonText}>Join an Existing Session</Text>
+          </Link>
+        </View>
+      </View>
     </View>
   );
 }
 
 export { supabase };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#74a1b5",
+    padding: 30,
+  },
+  headerSpacer: {
+    height: "5%",
+  },
+  header: {
+    height: "10%",
+    width: "100%",
+    backgroundColor: "#508991",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  buttonWrapper: {
+    backgroundColor: "#cde0c9",
+    borderRadius: 20,
+    padding: 20,
+    width: "80%",
+    alignItems: "center",
+    margin: 40,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#333",
+  },
+});
